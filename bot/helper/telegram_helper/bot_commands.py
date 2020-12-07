f00e249330cb1d4784cd296754ccc2e670d5750e
jikkubot <66976170+jikkubot@users.noreply.github.com>
2020-12-08 00:43:41 +0400

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -2,9 +2,14 @@ class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
         self.ListCommand = 'search'
-        self.AuthorizedUsersCommand = 'users'
+        self.StatusCommand = 'status'
         self.AuthorizeCommand = 'auth'
         self.UnAuthorizeCommand = 'unauth'
+        self.PingCommand = 'ping'
+        self.RestartCommand = 'restart'
+        self.StatsCommand = 'stats'
+        self.HelpCommand = 'help'
         self.LogCommand = 'log'
+        
 
 BotCommands = _BotCommands()
